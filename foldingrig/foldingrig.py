fd306e7039acf87c8972d84db50944116b389d6f
felix920506 <felix920506@gmail.com>
2023-01-14 11:00:39 -0500

    Password support, sync initial state

--- foldingrig/foldingrig.py
+++ foldingrig/foldingrig.py
@@ -2,43 +2,53 @@ import telnetlib
 import serial
 import json
 
+# Read Config
+
 with open('config.json','r',encoding='utf8') as configfile:
     options = json.load(configfile)
 
-host = '127.0.0.1:36330'
-enabled_slots = [0,2]
-controllerport = 'COM3'
-controllerbaud = 115200
+# host = '127.0.0.1:36330'
+# enabled_slots = [0,2]
+# controllerport = 'COM3'
+# controllerbaud = 115200
+
+# Setup FAH Connections
 
 fahclients = []
 
 class client:
-    def __init__(self,host,port,slots):
+    def __init__(self,host,port,slots,password=None):
         self.connection = telnetlib.Telnet(host,port)
         self.connection.read_until(b'>')
         self.slots = slots
+        if password:
+            self.run(f'auth {password}')
+
+    def run(self,cmd):
+        self.connection.write(bytes(cmd + '\n','utf8'))
+        return self.connection.read_unti(b'>')
 
     def pause(self):
         cmd = 'pause'
         for slot in self.slots:
             finalcmd = cmd + ' ' + str(slot) + '\n'
-            self.connection.write(bytes(finalcmd,'utf8'))
-            self.connection.read_until(b'>')
+            self.run(finalcmd)
     
     def unpause(self):
         cmd = 'unpause'
         for slot in self.slots:
             finalcmd = cmd + ' ' + str(slot) + '\n'
-            self.connection.write(bytes(finalcmd,'utf8'))
-            self.connection.read_until(b'>')
-
+            self.run(finalcmd)
 
 for host in options['hosts']:
     fahclients.append(client(host['ip'],host['port'],host['slots']))
 
+# setup MCU connection
+
 controller = serial.Serial(options['mcu']['port'], options['mcu']['baud'])
 
 lastState = False
+newState = False
 
 def pause():
     for host in fahclients:
@@ -48,15 +58,26 @@ def unpause():
     for host in fahclients:
         host.unpause()
 
+# Throw out MCU init info and get initial state on heating/idle
 
-# get initial slot info
+init_done = False
 
-# fahclient.write(b'slot-info\n')
-# fahclient.read_until(b'PyON 1 slots')
-# slots = eval(fahclient.read_until(b'\n---\n>')[:-6], {}, {})
+while not init_done:
+    try:
+        newState = int(controller.readline().decode('utf8').strip())
+    except:
+        pass
+    else:
+        init_done = True
 
-# for slot in slots:
-#     print(slot['options']['paused'])
+# Set FAHClient(s) to initial supposed to be state
+
+if newState:
+    unpause()
+else:
+    pause()
+
+# continuing to watch for state changes
 
 while True:
     try:
@@ -65,7 +86,7 @@ while True:
         continue
 
     if not newState == lastState:
-        if newState == True:
+        if newState:
             unpause()
         else:
             pause()
